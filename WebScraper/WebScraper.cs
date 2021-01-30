508a53b4f3641a8404fd9b44e70d7cdf041c3c4f
MatKapp <mateusz.kapiszewski@gmail.com>
2021-01-30 17:44:09 +0100

    initial commit, project structure

---
+++ WebScraper/WebScraper.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+
+namespace WebScraper
+{
+    public class WebScraper
+    {
+        public IBrowsingContext Context { get; set; }
+
+        public WebScraper()
+        {
+            CreateContext();
+        }
+
+        public async Task<IDocument> LoadDocument(string address)
+            => await Context.OpenAsync(address);
+
+        private void CreateContext()
+        {
+            var configuration = Configuration.Default.WithDefaultLoader();
+            Context = BrowsingContext.New(configuration);
+        }
+    }
+}
